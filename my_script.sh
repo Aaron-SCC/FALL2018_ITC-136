32e8a3d3125b871d88f4d9828cab0e799432b5ed
Aaron-SCC <aaron.lewis@seattlecentral.edu>
2018-10-29 20:46:31 -0700

    added functionality for pwd, version, and who logged in

--- my_script.sh
+++ my_script.sh
@@ -3,5 +3,21 @@
 echo "hello ITC-136"
 
 echo "edited in github"
+echo ""
+echo ""
+echo ""
+
+
+echo "        This is current working directory: "
+pwd
+echo ""
+
+echo "        This is the version of linux you are running: "
+uname -a
+echo ""
+
+echo "        These are your logged in users: "
+who
+echo ""
 
 
