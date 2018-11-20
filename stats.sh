d3792c4c8c42e5c05efbfda501f392e12b2e11e4
Aaron-SCC <32554879+Aaron-SCC@users.noreply.github.com>
2018-11-19 20:04:40 -0800

    Create stats.sh

---
+++ stats.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+diskused=$(df -h | grep '/dev/sda1'  | awk '{print $5}')
+
+
+
+
+memfree=$( free -mh | grep Mem: | awk '{print $7}'  )
+
+
+
+connections=$( netstat -tupn  | grep -v '^Active' | grep -v '^Proto' | wc -l  )
+
+
+users=$( who )
+
+
+
+echo "This is a snapshot of your current system:
+
+
+                     Disk Used:       $diskused
+
+                     Free Memory:     $memfree
+
+                     Logged in Users: $users
+
+Open Internet Connections:
+$connections
+
+ "
+
+
+
+
+echo "
+color info:
+
+http://misc.flogisoft.com/bash/tip_colors_and_formatting
+http://wiki.bash-hackers.org/scripting/style
+https://help.ubuntu.com/community/UnityLaunchersAndDesktopFiles
+
+"
+
+
+
+
+
